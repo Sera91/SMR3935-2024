d6975b5b5d7d06846aaef3268b785a4f844ebc35
Trung Nguyen <ndactrung@gmail.com>
2024-04-24 15:58:02 +0545

    Added vectorAdd CPU and matMult

---
+++ Day3/examples/cuda/vectorAdd_cpu.c
@@ -0,0 +1,81 @@
+/* Copyright (c) 2022, NVIDIA CORPORATION. All rights reserved.
+ */
+
+/**
+ * Vector addition: C = A + B.
+ *
+ * This sample is a very basic sample that implements element by element
+ * vector addition. It is the same as the sample illustrating Chapter 2
+ * of the programming guide with some additions like error checking.
+ */
+
+#include <stdio.h>
+#include <time.h>
+
+// For the CUDA runtime routines (prefixed with "cuda_")
+#include <cuda_runtime.h>
+
+//#include <helper_cuda.h>
+/**
+ * CUDA Kernel Device code
+ *
+ * Computes the vector addition of A and B into C. The 3 vectors have the same
+ * number of elements numElements.
+ */
+__global__ void vectorAdd(const float *A, const float *B, float *C,
+                          int numElements) {
+  int i = blockDim.x * blockIdx.x + threadIdx.x;
+
+  if (i < numElements) {
+    C[i] = A[i] + B[i];
+  }
+}
+
+/**
+ * Host main routine
+ */
+int main(void) {
+  // Error code to check return values for CUDA calls
+  cudaError_t err = cudaSuccess;
+
+  // Print the vector length to be used, and compute its size
+  int numElements = 50000;
+  size_t size = numElements * sizeof(float);
+  printf("[Vector addition of %d elements]\n", numElements);
+
+  // Allocate the host input vector A
+  float *h_A = (float *)malloc(size);
+
+  // Allocate the host input vector B
+  float *h_B = (float *)malloc(size);
+
+  // Allocate the host output vector C
+  float *h_C = (float *)malloc(size);
+
+  // Verify that allocations succeeded
+  if (h_A == NULL || h_B == NULL || h_C == NULL) {
+    fprintf(stderr, "Failed to allocate host vectors!\n");
+    exit(EXIT_FAILURE);
+  }
+
+  // Initialize the host input vectors
+  for (int i = 0; i < numElements; ++i) {
+    h_A[i] = rand() / (float)RAND_MAX;
+    h_B[i] = rand() / (float)RAND_MAX;
+  }
+
+  clock_t start = clock();
+  for (int i = 0; i < numElements; ++i) {
+    h_C[i] = h_A[i] + h_B[i];
+  }
+  double t = (float)(clock() - start)/CLOCKS_PER_SEC;
+  printf("Elapsed time = %f sec\n", t);
+
+  // Free host memory
+  free(h_A);
+  free(h_B);
+  free(h_C);
+
+  printf("Done\n");
+  return 0;
+}
