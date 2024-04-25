d5fe478efcfe808b9d031c1447805c63eae8fe20
Sera91 <serafina.digioia@inaf.it>
2024-04-25 08:08:36 +0200

    new file:   Intro-to-Python/Cupy-101/test-matmul/test-pytorch-matmul-VS-cupy-matmul.py 	new file:   Intro-to-Python/Cupy-101/test-matmul/timing-cupy-matmul.py 	modified:   launch_Jupyter.sh

---
+++ Day4/Intro-to-Python/Cupy-101/test-matmul/timing-cupy-matmul.py
@@ -0,0 +1,15 @@
+import cupy
+import cupyx
+import torch
+
+N = 1093
+x = cupy.random.rand(N, N, dtype=cupy.float32)
+y = cupy.random.rand(N, N, dtype=cupy.float32)
+
+cupy.cuda.device.get_cublas_handle()
+print(cupyx.time.repeat(cupy.core.matmul, (x, y), n_repeat=10))
+
+#x = torch.zeros((N, N), dtype=torch.float32, device='cuda:0')
+#y = torch.zeros((N, N), dtype=torch.float32, device='cuda:0')
+
+#print(cupyx.time.repeat(torch.matmul, (x, y), n_repeat=10))
